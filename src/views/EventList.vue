e22c659d9cf78ba9a7b559027364fb3b3b4e085f
saeedmaldosary <saeedmaldosary@gmail.com>
2021-05-08 20:53:49 +0300

    Initial commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Event Listing</h1>
+    <EventCard
+      v-for="event in events"
+      :key="event.id"
+      :event="event"
+    ></EventCard>
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+
+export default {
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => {
+        this.events = response.data // For now, logs out the response
+      })
+      .catch((error) => {
+        console.log('There was an error:', error.response) // Logs out the error
+      })
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/EventShow.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ event.title }}</h1>
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService.js'
+export default {
+  props: ['id'],
+  data() {
+    return {
+      event: {}
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((response) => {
+        this.event = response.data // For now, logs out the response
+        console.log(this.event)
+      })
+      .catch((error) => {
+        console.log('There was an error:', error.response) // Logs out the error
+      })
+  }
+}
+</script>
